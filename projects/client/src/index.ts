1e753d1527a7c267b5188b587eaf3b81cd55eb1a
shahar <shahar@hyperguest.com>
2025-06-20 17:17:36 +0300

    feat(boilerplate): initial commit

---
+++ projects/client/src/index.ts
@@ -0,0 +1,3 @@
+export function GetGreeting() {
+    return 'Hello World from server!';
+}
---
+++ projects/server/src/test/register.js
@@ -0,0 +1,19 @@
+/**
+ * Overrides the tsconfig used for the app.
+ * In the test environment we need some tweaks.
+ */
+
+/* eslint-disable */
+const tsNode = require("ts-node");
+
+delete process.env.NODE_ENV;
+process.env.IS_TEST = true;
+
+/* eslint-disable */
+module.exports = () => {
+  tsNode.register({
+    files: true,
+    transpileOnly: true,
+    project: "./tsconfig.json",
+  });
+};
