037c7898845b6efba503e03f9c695129a9900f56
shahar <shahar@hyperguest.com>
2025-06-21 00:37:19 +0300

    test

--- projects/server/src/a.test.ts
+++ projects/server/src/a.test.ts
@@ -2,7 +2,7 @@ import { GetGreeting } from ".";
 
 describe("GetGreeting", () => {
   it("should return a greeting message", () => {
-    const name = "World";
+    const name = "world";
     const expectedGreeting = `Hello ${name}!`;
     expect(GetGreeting(name)).toBe(expectedGreeting);
   });
